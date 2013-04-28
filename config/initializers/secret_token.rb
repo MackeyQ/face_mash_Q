8657fab5bbf3f2f97c7d932e08c312f3c920bc47
MakeyQ <2172897@qq.com>
2013-04-28 23:23:11 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FaceMashQ::Application.config.secret_token = 'b3b6d5aa94d0d7d84c2bb64b8b1642952f0e15c907b87590111d26cc1e930f85bae786111643de2dcaf2bd2f6fe92833a9949c7da826c2e9f9e50a2c9b19a868'
